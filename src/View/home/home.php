4e0d40fa1aaed6ca8b110c1be5a544412cdc53e4
Fatah-DERRADJ <derradjfatah@gmail.com>
2019-05-26 21:00:28 +0200

    installation template bootstrap sur template et home

--- src/View/home/home.php
+++ src/View/home/home.php
@@ -2,6 +2,72 @@
 ob_start();
 ?>
 
+<section class="section-4">
+    <div class="container">
+        <div class="row heading">
+            <div class="col-sm-6 col-12">
+                <h3>Read our articles<span>We write beautiful blog</span></h3>
+            </div>
+            <div class="col-sm-6 col-12">
+                <a href="blog.html" class="btn btn-success">Read our full blog</a>
+            </div>
+        </div>
+        <!--/row-->
+        <div class="row">
+            <div class="col-lg-4 col-sm-12 col-12 box-1"  data-aos="fade-right" data-aos-delay="300">
+                <figure class="figure">
+                    <a href="blog.html"><img src="images/blog-1.jpg" class="figure-img img-fluid" alt="blog"></a>
+                    <figcaption class="figure-caption">
+                        <h2><a href="blog.html">Beautiful girl holding her camera in neck for a photo shoot</a></h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</p>
+                        <a href="blog_single.html" class="btn btn-success">+ more</a>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-8 col-sm-12 col-12" data-aos="fade-left" data-aos-delay="300">
+                <div class="row" >
+                    <div class="col-sm-6 col-12 box-2">
+                        <figure class="figure">
+                            <a href="blog.html"><img src="images/blog-2.jpg" class="figure-img img-fluid" alt="blog"></a>
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12 box-3">
+                        <h4><a href="blog.html">Girl working seriously on laptop in office hours</a></h4>
+                        <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. specimen book. It has survived not only five</h5>
+                        <a href="blog_single.html" class="btn btn-success">+ more</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-12 box-4">
+                        <figure class="figure">
+                            <a href="blog.html"><img src="images/blog-3.jpg" class="figure-img img-fluid" alt="blog"></a>
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12 box-5">
+                        <h4><a href="blog.html">Girl working seriously on laptop in office hours </a></h4>
+                        <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. specimen book. It has survived not only five</h5>
+                        <a href="blog_single.html" class="btn btn-success">+ more</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-12 box-6">
+                        <figure class="figure">
+                            <a href="blog.html"><img src="images/blog-1.jpg" class="figure-img img-fluid" alt="blog"></a>
+                        </figure>
+                    </div>
+                    <div class="col-sm-6 col-12 box-7">
+                        <h4><a href="blog.html">Girl working seriously on laptop in office hours </a></h4>
+                        <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. specimen book. It has survived not only five</h5>
+                        <a href="blog_single.html" class="btn btn-success">+ more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--container-->
+
+
+
 <h1>Hello world !</h1>
 <?php if(isset($_SESSION['flash'])) : ?>
 <p><?= $_SESSION['flash']; ?></p>
@@ -31,6 +97,30 @@ ob_start();
     <a href="chapitre&id=<?= $data->getId();?>"> Voir la suite </a>
     <?php endforeach; ?>
 </div>
+
+    <section class="section-6" data-aos="fade-up">
+        <div class="container">
+            <!-- Grid row-->
+            <div class="row main" data-aos="fade-up" data-aos-delay="300">
+                <!-- Grid column -->
+                <div class="col-lg-6 col-sm-12 col1">
+                    <div class="heading">
+                        <h3>Subscribe to Newsletter</h3>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 col1">
+                    <form>
+                        <div class="input-group">
+                            <input name="email" id="email" type="email" placeholder="Enter your email id" required>
+                            <button class="btn btn-info" type="submit">Subscribe</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 
 <?php
 $content = ob_get_clean();
--- src/View/template.php
+++ src/View/template.php
@@ -1,15 +1,255 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css">
+    <link rel="stylesheet" href="public/css/animate.css">
+    <link rel="stylesheet" href="public/css/main.css">
+
+
+
     <title>"Projet 3"</title>
 </head>
+
 <body>
+
+<header class="header bg">
+    <div class="container text-white">
+        <div class="row">
+            <div class="col-sm-4 align-self-center text-left">
+                <!--<h6>Estd 1905</h6>--!>
+            </div>
+            <div class="col-sm-4 col-12 align-self-center box-1 text-center">
+                <a class="navbar-brand" href="accueil"><img src="public/image/header-logo.jpg" alt="logo"></a>
+            </div>
+            <div class="col-sm-4 align-self-center text-right">
+                <div class="social-icons">
+                    <a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+                    <a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+        <!--/row-->
+    </div>
+    <!--container-->
+</header>
+<span class="position-absolute trigger"><!-- hidden trigger to apply 'stuck' styles --></span>
+<nav class="navbar navbar-expand-sm sticky-top navbar-dark">
+    <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar1">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link active" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="portfolio.html">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="services.html">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Support</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.html">blog</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--container end-->
+</nav>
+<!--Section-1-->
+<section class="section-1">
+    <div class="jumbotron d-flex align-items-center">
+        <div class="gradient"></div>
+        <div class="container-fluid content">
+            <h1 data-aos="fade-up" data-aos-delay="100">Welcome to more.</h1>
+            <h2 data-aos="fade-up" data-aos-delay="300">the multipurpose psd wordrpess theme</h2>
+            <h4 data-aos="fade-up" data-aos-delay="500">Getup and rise, for the new chapter of your life</h4>
+            <p data-aos="fade-up" data-aos-delay="700"><a href="about.html" class="btn btn-success">Get Started</a></p>
+        </div>
+        <!--container-fluid end-->
+    </div>
+</section>
+
+
+
+
 <?= $content; ?>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<section class="section-7">
+    <!-- Footer -->
+    <footer class="page-footer font-small stylish-color-dark">
+
+        <!-- Footer Links -->
+        <div class="container text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-lg-5 col-sm-12 mx-auto box-1">
+
+                    <!-- Content -->
+                    <a href="index.html"><img src="public/image/footer-logo.png" alt="footer-logo"></a>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</p>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none">
+
+                <!-- Grid column -->
+                <div class="col-lg-2 col-sm-4 box-2 mx-auto">
+
+                    <!-- Links -->
+                    <h5>Site Navigation</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="index.html">go to home</a>
+                        </li>
+                        <li>
+                            <a href="services.html">services</a>
+                        </li>
+                        <li>
+                            <a href="portfolio.html">portfolio</a>
+                        </li>
+                        <li>
+                            <a href="blog.html">read blog</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">contact us</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">product support</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none">
+
+                <!-- Grid column -->
+                <div class="col-lg-2 col-sm-4 box-3 mx-auto">
+
+                    <!-- Links -->
+                    <h5>information</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">user login</a>
+                        </li>
+                        <li>
+                            <a href="#!">creat new account</a>
+                        </li>
+                        <li>
+                            <a href="#!">checkout</a>
+                        </li>
+                        <li>
+                            <a href="#!">my cart</a>
+                        </li>
+                        <li>
+                            <a href="#!">other information</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none">
+
+                <!-- Grid column -->
+                <div class="col-lg-2 col-sm-4 box-4 mx-auto">
+
+                    <!-- Links -->
+                    <h5>policies & Info</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Terms Conditions </a>
+                        </li>
+                        <li>
+                            <a href="#!">website polocy</a>
+                        </li>
+                        <li>
+                            <a href="#!">Policy for Sellers </a>
+                        </li>
+                        <li>
+                            <a href="#!">Policy for Buyers</a>
+                        </li>
+                        <li>
+                            <a href="#!">Shipping & Refund </a>
+                        </li>
+                        <li>
+                            <a href="#!">Wholesale Policy</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center">
+            <div class="gradient"></div>
+            <p>© 2019, All Rights reserved. designed by<a href="#"> DERRADJ Fatah</a></p>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+</section>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+<script>
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+</script>
+
 </body>
 </html>
+
+
+
+
+
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+--!>
+
