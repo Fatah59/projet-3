4d652a3503b45ce51367aaafbcbc281323de72c3
Fatah-DERRADJ <derradjfatah@gmail.com>
2019-06-26 17:20:24 +0200

    mise en place du système de mail de contact via phpmailer

--- src/Model/ContactManager.php
+++ src/Model/ContactManager.php
@@ -13,5 +13,14 @@ class ContactManager extends DbManager
         $this->db = self::dbConnect();
     }
 
+    public function addContact(Contact $contact){
+        $req=$this->db->prepare('INSERT INTO contact(username, userMessage, email, sendDate, processed, consent) VALUES (:username, :userMessage, :email, NOW(), FALSE, TRUE)');
+        $addcontact=$req->execute([
+            'username'=>$contact->getUsername(),
+            'userMessage'=>$contact->getUserMessage(),
+            'email'=>$contact->getEmail(),
+        ]);
+        return $addcontact;
+    }
 
 }
---
+++ src/Service/Login.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Service;
+
+
+class Login
+{
+
+    const EMAIL= 'derradjfatah@gmail.com';
+    const PASSWORD= '12ya46fh78';
+}
